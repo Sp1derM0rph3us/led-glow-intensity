95ea4cc309135c99c9b133ec5b42c72141fd1e67
444 <aiongremory@protonmail.com>
2023-04-14 02:06:45 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 // C++ cod
 // Created by sp1d3rm0rph3us
 
-const int led = 3; // Setting what port the led is
+const int led = 3; // defining in wich port the led is connected to
 int value = 0; // Creating a variable for the ammount of power
 
 void setup()
@@ -15,14 +15,14 @@ void setup()
 void loop()
 {
   if (Serial.available() > 0) {	// If the value of "value" variable is bigger than zero
-    value = Serial.parseInt();	// reads the value inside "value"
+    value = Serial.parseInt();
     if(value <= 255) {		// Checks if the value of it isn't bigger than 255
-      analogWrite(led, value);		// Writes to the LED that value
+      analogWrite(led, value);		// Write the value "to" the LED
       Serial.println(String("LED power: ") + value);	// Outputs LED's power
     }
-    else{
+    else{   // Treating what happens if the value is bigger than 255
       analogWrite(led, 0);
-      Serial.println(String("You've inserted a value bigger then the allowed, setting LED's power to 0."));
+      Serial.println(String("You've inserted a value bigger than the allowed, setting LED's power to 0."));
     }
   }
 }
